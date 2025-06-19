0ff4bee85a13e541df6254e49515505be13d187f
Vidith <kotasaividith@gmail.com>
2025-06-19 16:59:52 +0530

    Update product.java

--- Week1/EcommerceSearchFunction/product.java
+++ Week1/EcommerceSearchFunction/product.java
@@ -1,1 +1,12 @@
+public class Product {
+    int productId;
+    String productName;
+    String category;
+
+    public Product(int id, String name, String cat) {
+        this.productId = id;
+        this.productName = name;
+        this.category = cat;
+    }
+}
 
